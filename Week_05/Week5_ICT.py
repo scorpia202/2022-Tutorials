a0d683498fff791af7e2be4e857452fd57593480
Liam H <109998079+scorpia202@users.noreply.github.com>
2022-08-24 13:41:33 +1000

    Update Week5_ICT.py

--- Week_05/Week5_ICT.py
+++ Week_05/Week5_ICT.py
@@ -1,11 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.loadtxt("data_points.txt")
 
 
 x, f = np.loadtxt("data_points.txt",unpack = True)
-
+data = np.column_stack((x,f))
 
 X = np.atleast_2d(x).T
 F = np.atleast_2d(f).T
@@ -13,4 +12,17 @@ p0 = x**0
 p1 = x**1
 p2 = x**2
  
-p = np.column_stack(p0,p1,p2)
+P = np.column_stack((p0,p1,p2))
+
+a = np.linalg.inv(P.T @ P) @ P.T @ F
+
+fitted_trend = a[0] +a[1] * x+a[2] *x**2
+
+smooth_x = np.linspace(0,3)
+fitted_trend = a[0] +a[1] *smooth_x + a[2] *smooth_x**2
+
+fig, ax = plt.subplots()
+ax.scatter(x,f)
+ax.plot(smooth_x, fitted_trend, 'r')
+
+plt.show()
